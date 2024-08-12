5202dccc97fb67bc911da99f71cd612a6afab500
ouo314 <96057963+ouo314@users.noreply.github.com>
2024-08-12 15:38:11 +0800

    Draw the diffraction pattern and remove the waves that extend beyond the frame.

--- index.js
+++ index.js
@@ -27,16 +27,40 @@ const waveL = 10; // 波長
 const numWaves = (srcY - 10) / waveL; // 波前數量
 const waveSpacing = waveL; // 波前間距
 
+let r = waveL;
+let turY = barY - waveL;
+const tur = new bt.Turtle();
+tur.jump([gapL, barY - waveL]);
+tur.right(180);
 
 for (let i = 0; i < numWaves; i++) {
   const yOffset = srcY - (i + 1) * waveSpacing;
   const line = [];
-  const circle = [];
   
+  tur.up();
+  tur.jump([gapL, turY]);
+  tur.setAngle(180);
+  tur.down();
+  tur.arc(-90, r);
+  const path = tur.path;
+
+  const filteredPath = bt.iteratePoints(path, (pt) => {
+    const [x, y] = pt;
+
+    if (x <= 0 || y <= 0) {
+      return "REMOVE";
+    }
+    return [x, y];
+  });
+
+  drawLines(filteredPath);
+
+  r += waveL;
+  turY -= waveL;
+
   for (let x = 0; x <= width; x++) {
     let blocked = false;
     if (yOffset <= barY) {
-      
       if (x > gapR || x < gapL) {
         blocked = true;
       }
@@ -48,6 +72,6 @@ for (let i = 0; i < numWaves; i++) {
   }
 
   if (line.length > 0) {
-    drawLines([line], { fill: "yellow" });
+    drawLines([line]);
   }
 }
